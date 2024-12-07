4a111f07bd20444901a6dc34f91e3c3b80252c9e
Klaudia <klaudia134000@wp.pl>
2024-12-07 15:37:37 +0100

    ImageProcesor - adding moveForward option and lost line handler

--- src/main/java/org/example/Camera/ImageProcesor.java
+++ src/main/java/org/example/Camera/ImageProcesor.java
@@ -75,120 +75,129 @@ public class ImageProcesor {
 
         //endregion
 
-        //region Skrety
+        //region Ruch do przodu / zjechanie ze sciezki
+        int SPixel = (int) binaryFrame.get(midY, midX)[0];
+        if (SPixel == 255) {
+            robotController.setMovmentSpeed(50, 50);
+        } else {
 
+            int closestX = -1;
+            int closestY = -1;
+            double minDistance = Double.MAX_VALUE;
+
+            // Przeszukiwanie obrazu w celu znalezienia najbliższego białego piksela
+            for (int y = 0; y < binaryFrame.rows(); y++) {
+                for (int x = 0; x < binaryFrame.cols(); x++) {
+                    if ((int) binaryFrame.get(y, x)[0] == 255) { // Jeśli piksel jest biały
+                        // Oblicz odległość euklidesową od centralnego punktu (midX, midY)
+                        double distance = Math.sqrt(Math.pow(midX - x, 2) + Math.pow(midY - y, 2));
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            closestX = x;
+                            closestY = y;
+                        }
+                    }
+                }
+
+            }
+            if (closestX != -1 && closestY != -1) {
+                System.out.println("Najbliższy biały piksel znaleziony na pozycji: (" + closestX + ", " + closestY + ").");
+            }
+
+            // Określenie kierunku na podstawie sekcji, w której znajduje się najbliższy biały piksel
+            if (closestX < midX && closestY < midY) { // Sekcja A
+                robotController.setMovmentSpeed(-30, 30); // Skręt w lewo
+            } else if (closestX >= midX && closestY < midY) { // Sekcja B
+                robotController.setMovmentSpeed(30, -30); // Skręt w prawo
+            } else if (closestX < midX && closestY >= midY) { // Sekcja C
+                robotController.setMovmentSpeed(-50, 50); // Szybszy skręt w lewo
+            } else if (closestX >= midX && closestY >= midY) { // Sekcja D
+                robotController.setMovmentSpeed(50, -50); // Szybszy skręt w prawo
+            }
+
+        }
+
+            //endregion
         String comparisonResult;
 
-        //region TODO Punkt srodkowy - strasznie glupie - metoda szukaj trasy (patrz lewo, prawo, do przodu)
-
-        // Zliczanie drogi poza czarnymi pikselami
-        int distanceTravelled = 0;
-        boolean onLine = true; // robot na linii
-
-        // Sprawdzenie, czy punkt przecięcia (środek obrazu) ma czarne piksele
-        int SPixel = (int) binaryFrame.get(midY, midX)[0]; // Sprawdzamy wartość piksela
-
-        if (SPixel == 0) { // 0 to czarna wartość piksela
-            distanceTravelled++;
-            System.out.println("dT: " + distanceTravelled);
-            robotController.emergencyStop();
-            robotController.moveReverse(); //TODO w parametrze przekazac distanceTravelled
-        } else {
-            distanceTravelled = 0;  // Resetujemy zliczanie drogi
-            onLine = true;
-        }
-
-        //endregion
-
-
-        //region TODO moveForward()
-
-        //endregion
-
-        //punkt srodkowy szuka czarnych pikseli
-        //jesli prawo to B czy D, na tej podstawie ustala sile silnikow (skret)
-        // Porównanie: Jeśli po lewej stronie (A + C) więcej czarnych pikseli
-
-        // Lewa strona (A + C)
-        if (leftBlackPixels > rightBlackPixels) {
-            comparisonResult = "A+C lewo";  // Więcej czarnych pikseli po lewej stronie
-            if(blackPixelsA > blackPixelsC)
-            {
-                //TODO skrec w lewo - maly kat
-                robotController.delay(20);
+            // Lewa strona (A + C)
+            if (leftBlackPixels > rightBlackPixels) {
+                comparisonResult = "A+C lewo";  // Więcej czarnych pikseli po lewej stronie
+                if (blackPixelsA > blackPixelsC) {
+                    comparisonResult = "A";
+                    robotController.setMovmentSpeed(40, 50);
+                    robotController.delay(20);
+                } else if (blackPixelsC > blackPixelsA) {
+                    robotController.setMovmentSpeed(30, 70);
+                }
             }
-            else if(blackPixelsC > blackPixelsA){
-                robotController.turnLeft();
-                //TODO skrec w lewo szybciej - duzy kat
-            }
-        }
-        // Prawa strona (B + D)
-        else if (rightBlackPixels > leftBlackPixels) {
-            comparisonResult = "B+D prawo";  // Więcej czarnych pikseli po prawej stronie
-            robotController.turnRight();
-            if(blackPixelsB > blackPixelsD)
-            {
-                //TODO skrec w prawo - maly kat
+
+            // Prawa strona (B + D)
+            else if (rightBlackPixels > leftBlackPixels) {
+                comparisonResult = "B+D prawo";  // Więcej czarnych pikseli po prawej stronie
                 robotController.turnRight();
+                if (blackPixelsB > blackPixelsD) {
+                    comparisonResult = "B";
+//                robotController.turnRight();
+                    robotController.setMovmentSpeed(50, 40);
+                } else if (blackPixelsD > blackPixelsB) {
+                    comparisonResult = "D";
+//                robotController.turnRight();
+                    robotController.setMovmentSpeed(70, 30);
+                }
+            } else {
+                comparisonResult = "same black pixels value";
+                robotController.moveForward(); // maja tyle samo
             }
-            else if(blackPixelsD > blackPixelsB)
-            {
-                //TODO skrec w prawo szybciej - duzy kat
-                robotController.turnRight();
+
+
+            //endregion
+
+            //region Rysowanie linii i etykiet
+
+            Imgproc.line(outputFrame, new Point(midX, 0), new Point(midX, height), new Scalar(0, 0, 255), 2); // Pionowa linia
+            Imgproc.line(outputFrame, new Point(0, midY), new Point(width, midY), new Scalar(0, 0, 255), 2); // Pozioma linia
+
+            Imgproc.putText(outputFrame, "A", new Point(midX / 2, midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
+            Imgproc.putText(outputFrame, "B", new Point(midX + midX / 2, midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
+            Imgproc.putText(outputFrame, "C", new Point(midX / 2, midY + midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
+            Imgproc.putText(outputFrame, "D", new Point(midX + midX / 2, midY + midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
+
+            Imgproc.putText(outputFrame, comparisonResult, new Point(10, 30), Imgproc.FONT_HERSHEY_SIMPLEX, 0.8, new Scalar(0, 255, 0), 2);
+
+            //endregion
+
+            return outputFrame;
+
+        }
+
+        public Mat proses (Mat frame){
+
+            Mat grayFrame = new Mat();
+            Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+
+            // Binaryzacja obrazu: szukamy czarnych obiektów (jasne tło -> ciemne obiekty)
+            Mat binaryFrame = new Mat();
+            Imgproc.threshold(grayFrame, binaryFrame, 50, 255, Imgproc.THRESH_BINARY_INV);
+
+            // Znajdowanie konturów czarnych obiektów
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(binaryFrame, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            // Iteracja przez znalezione kontury
+            for (int i = 0; i < contours.size(); i++) {
+                Rect boundingBox = Imgproc.boundingRect(contours.get(i));
+                if (boundingBox.width > 10 && boundingBox.height > 10) {
+                    Imgproc.rectangle(frame, boundingBox, new Scalar(0, 255, 0), 2);
+
+                    //System.out.println("X: "+boundingBox.x + "\n");
+
+                    if (boundingBox.x >= 15 && boundingBox.x <= 600) robotController.moveForward();
+                    else if (boundingBox.x < 15) robotController.turnLeft();
+                    else if (boundingBox.x > 600) robotController.turnRight();
+                }
             }
+            return frame;
         }
-        else {
-            comparisonResult = "same black pixels value";
-            robotController.moveForward(); // maja tyle samo
-        }
-
-
-        //endregion
-
-        //region Rysowanie linii i etykiet
-
-        Imgproc.line(outputFrame, new Point(midX, 0), new Point(midX, height), new Scalar(0, 0, 255), 2); // Pionowa linia
-        Imgproc.line(outputFrame, new Point(0, midY), new Point(width, midY), new Scalar(0, 0, 255), 2); // Pozioma linia
-
-        Imgproc.putText(outputFrame, "A", new Point(midX / 2, midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
-        Imgproc.putText(outputFrame, "B", new Point(midX + midX / 2, midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
-        Imgproc.putText(outputFrame, "C", new Point(midX / 2, midY + midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
-        Imgproc.putText(outputFrame, "D", new Point(midX + midX / 2, midY + midY / 2), Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 0, 255), 2);
-
-        Imgproc.putText(outputFrame, comparisonResult, new Point(10, 30), Imgproc.FONT_HERSHEY_SIMPLEX, 0.8, new Scalar(0, 255, 0), 2);
-
-        //endregion
-
-        return outputFrame;
     }
-
-    public Mat proses(Mat frame){
-
-        Mat grayFrame = new Mat();
-        Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
-
-        // Binaryzacja obrazu: szukamy czarnych obiektów (jasne tło -> ciemne obiekty)
-        Mat binaryFrame = new Mat();
-        Imgproc.threshold(grayFrame, binaryFrame, 50, 255, Imgproc.THRESH_BINARY_INV);
-
-        // Znajdowanie konturów czarnych obiektów
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(binaryFrame, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        // Iteracja przez znalezione kontury
-        for (int i = 0; i < contours.size(); i++) {
-            Rect boundingBox = Imgproc.boundingRect(contours.get(i));
-            if (boundingBox.width > 10 && boundingBox.height > 10) {
-                Imgproc.rectangle(frame, boundingBox, new Scalar(0, 255, 0), 2);
-
-                //System.out.println("X: "+boundingBox.x + "\n");
-
-                if(boundingBox.x >= 15 && boundingBox.x <= 600) robotController.moveForward();
-                else if (boundingBox.x < 15) robotController.turnLeft();
-                else if(boundingBox.x > 600) robotController.turnRight();
-            }
-        }
-        return frame;
-    }
-}
